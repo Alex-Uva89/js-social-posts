476c621da215685bb911c5a1b4522246481c252b
ALESSANDROUVA <a.uva1988@gmail.com>
2022-03-08 18:21:39 +0100

    correction

--- main.js
+++ main.js
@@ -80,58 +80,54 @@ for (let indicePost = 0; indicePost < posts.length; indicePost++) { // uso il mi
 };
 //ciclo per aggiungere ad ogni card l'evento di colorare il mi piace
 const btnLike = document.querySelectorAll('.js-like-button');
-const btnCount = document.getElementById('like-counter-1')
+let btnCount = document.querySelectorAll('.js-likes-counter');
+const arrIdLike = [];
 for (let indiceEvent = 0; indiceEvent < btnLike.length; indiceEvent++){
     let countInput = posts[indiceEvent].likes;
     btnLike[indiceEvent].addEventListener('click', function(){
         btnLike[indiceEvent].classList.toggle('like-button--liked');
         countInput++;
-        console.log(countInput)
-        btnCount.innerHTML = countInput;
+        btnCount[indiceEvent].innerHTML = countInput;
         countInput--;
     });
     
     console.log(countInput);
 }
 
-
-
-
-
-
+console.log(arrIdLike)
 console.log(btnLike)
 
 
 function createCard(posts){ 
-        areaStampCard.innerHTML += `<div class="post">
-        <div class="post__header">
-            <div class="post-meta">                    
-                <div class="post-meta__icon">
-                    <img class="profile-pic" src=${posts.author.image} alt=${posts.author.name}>                    
-                </div>
-                <div class="post-meta__data">
-                    <div class="post-meta__author">${posts.author.name}</div>
-                    <div class="post-meta__time">${posts.created}</div>
-                </div>                    
-            </div>
-        </div>
-        <div class="post__text">${posts.content}</div>
-        <div class="post__image">
-            <img src=${posts.media} alt="">
-        </div>
-        <div class="post__footer">
-            <div class="likes js-likes">
-                <div class="likes__cta">
-                    <a class="like-button  js-like-button" href="#" data-postid="1">
-                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                        <span class="like-button__label">Mi Piace</span>
-                    </a>
-                </div>
-                <div class="likes__counter">
-                    Piace a <b id="like-counter-1" class="js-likes-counter">${posts.likes}</b> persone
-                </div>
-            </div> 
-        </div>            
+    areaStampCard.innerHTML += `<div class="post">
+    <div class="post__header">
+    <div class="post-meta">                    
+    <div class="post-meta__icon">
+    <img class="profile-pic" src=${posts.author.image} alt=${posts.author.name}>                    
+    </div>
+    <div class="post-meta__data">
+    <div class="post-meta__author">${posts.author.name}</div>
+    <div class="post-meta__time">${posts.created}</div>
+    </div>                    
+    </div>
+    </div>
+    <div class="post__text">${posts.content}</div>
+    <div class="post__image">
+    <img src=${posts.media} alt="">
+    </div>
+    <div class="post__footer">
+    <div class="likes js-likes">
+    <div class="likes__cta">
+    <a class="like-button  js-like-button" href="#" data-postid="1">
+    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+    <span class="like-button__label">Mi Piace</span>
+    </a>
+    </div>
+    <div class="likes__counter">
+    Piace a <b id="like-counter-1" class="js-likes-counter">${posts.likes}</b> persone
+    </div>
+    </div> 
+    </div>            
     </div>`; 
     
 };
